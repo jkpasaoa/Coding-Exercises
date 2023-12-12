723b2f6058be33a4ddd995ca94b33f216193c277
Jacqueline Pasaoa <jacqueline.pasaoa@gmail.com>
2023-12-12 11:44:31 -0500

    solution to well of ideas

--- Codewars/Well-of-ideas.js
+++ Codewars/Well-of-ideas.js
@@ -5,5 +5,18 @@
 
 //Solution
 function well(x){
+  let count = 0;
 
+  for(let i = 0; i < x.length; i++) {
+    if(x[i] === 'good'){
+      count++
+    }
+  }
+  if(count >= 1 && count <= 2) {
+    return 'Publish!'
+  } else if (count > 2) {
+    return 'I smell a series!'
+  } else {
+    return 'Fail!'
+  }
 }
